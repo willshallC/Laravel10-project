2ef1c1e7ad953afe6baed354a2fd2a469d07e0f5
willshallC <netcreaterss@gmail.com>
2023-07-12 09:47:41 +0530

    added condition of empty products data

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,37 +3,40 @@
 @section('content')    
 
 <div class="products-container">
-    @foreach ($products as $product)
-        <div class="cmn-products">
-            <div class="product-img">
-                <img src="{{$product->product_img}}" alt="cat-img"/>
-            </div>
-            <div class="product-name">{{$product->product_name}}</div>
-            <div class="product-brand"><b>{{$product->product_brand}}</b></div>
-            <div class="product-price-view">
-                <span class="price"><strong><sup>$</sup>{{$product->product_price}}</strong></span>
-                <span class="view n">Quick View</span>
-            </div>
-            {{-- Popup --}}
-            <div class="modal" id="">
-                <div class="m-header">
-                    <div class="title">
-                        <h3>Description</h3>
-                    </div>
-                    <button onclick="togglePop()" class="close">&times;</button>
+    @if (count($products)<1)
+        <h1>No Products</h1>
+    @else    
+        @foreach ($products as $product)
+            <div class="cmn-products">
+                <div class="product-img">
+                    <img src="{{$product->product_img}}" alt="cat-img"/>
                 </div>
-                <div class="m-body">
-                    <div class="m-img mcmn"><img src="{{$product->product_img}}" alt="cat-img"/></div>
-                    <div class="m-desc mcmn">
-                        <h3>Product Description</h3>
-                        <p>{{$product->product_description}}</p>
+                <div class="product-name">{{$product->product_name}}</div>
+                <div class="product-brand"><b>{{$product->product_brand}}</b></div>
+                <div class="product-price-view">
+                    <span class="price"><strong><sup>$</sup>{{$product->product_price}}</strong></span>
+                    <span class="view n">Quick View</span>
+                </div>
+                {{-- Popup --}}
+                <div class="modal" id="">
+                    <div class="m-header">
+                        <div class="title">
+                            <h3>Description</h3>
+                        </div>
+                        <button onclick="togglePop()" class="close">&times;</button>
+                    </div>
+                    <div class="m-body">
+                        <div class="m-img mcmn"><img src="{{$product->product_img}}" alt="cat-img"/></div>
+                        <div class="m-desc mcmn">
+                            <h3>Product Description</h3>
+                            <p>{{$product->product_description}}</p>
+                        </div>
                     </div>
                 </div>
+                <div id="overlay" onclick="togglePop()"></div>
             </div>
-            <div id="overlay" onclick="togglePop()"></div>
-        </div>
-    @endforeach
-    
+        @endforeach
+    @endif   
 </div>
 
 @endsection
