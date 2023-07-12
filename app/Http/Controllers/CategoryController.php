98a13d815626b3777b2adb6f33e870ec77a8c415
willshallC <netcreaterss@gmail.com>
2023-07-12 09:19:40 +0530

    fixed subcategory to products slug

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -53,20 +53,21 @@ class CategoryController extends Controller
 
         if($category != null){
 
+            $catID = "";
             if($sub_category == null){
 
                 $string = strtolower(trim($category));
                 $cleanCategory = preg_replace('/[^A-Za-z0-9]+/', ' ', $string);
                 
                 $categoryID = DB::table('categories')->where('cat_name',$cleanCategory)->pluck('id');
-                $id =  $categoryID[0];
-                $sub_categories = DB::table('sub_categories')->where('parent_id','=',$id)->get();
+                $catID =  $categoryID[0];
+                $sub_categories = DB::table('sub_categories')->where('parent_id','=',$catID)->get();
 
                 // $cat_name = DB::table('categories')->where('id','=',$id)->pluck('cat_name');
                 $count = count($sub_categories);
                 if($count == 0){
 
-                    return redirect("product/{$id}");
+                    return redirect("product/{$catID}");
 
                 }else{
                     //return view('sub-categories',['sub_categories'=>$sub_categories,'cat_name'=>$cat_name]);
@@ -80,25 +81,22 @@ class CategoryController extends Controller
 
                 $string = strtolower(trim($sub_category));
                 $cleanSubCategory = preg_replace('/[^A-Za-z0-9]+/', ' ', $string);
-                return $cleanSubCategory;
-
-                $sub_categories = DB::table('sub_categories')->where('parent_id','=',$id)->get();
-
-                // $sub_categories = DB::select("select *, count(DISTINCT p.product_name) FROM sub_categories as sc join products as p  WHERE p.fcid = $id");
-                // return $sub_categories;
-                $cat_name = DB::table('categories')->where('id','=',$id)->pluck('cat_name');
-                $count = count($sub_categories);
-                if($count == 0){
-
-                    return redirect("product/{$id}");
-
-                }else{
-                    //return view('sub-categories',['sub_categories'=>$sub_categories,'cat_name'=>$cat_name]);
-                    // return view('sub-categories')->with('sub_categories',$sub_categories)->with('cat_name',$cat_name);
-                    
-                    return view('sub-categories',['sub_categories'=>$sub_categories,'cat_name'=>$cat_name]);
+                
+                $subCatId = DB::table('sub_categories')->where('sub_cat_name','=',$cleanSubCategory)->pluck('id');
+                $subID = $subCatId[0];
+                
+                $products = DB::table('products')->where('fscid','=',$subID)->get();
+                $products_count = count($products);
+                if($products_count>0){
+                    return view('product',['products'=>$products]);
                 }
-                //return view('sub-categories',['sub_categories'=>$sub_categories]);
+                else{
+                    $products = DB::table('products')->where('fcid','=',$catID)->get();
+                    return view('product',['products'=>$products]);
+                    
+                }
+                //return view('product',['products'=>$products]);
+            
             }
         }
     }
