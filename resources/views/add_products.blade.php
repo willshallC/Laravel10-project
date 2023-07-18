2ffe24da18ebc6cb144ae61d03c381146606974d
willshallC <netcreaterss@gmail.com>
2023-07-18 08:49:29 +0530

    fixed category and subcategory dropdown relation on product form

--- resources/views/add_products.blade.php
+++ resources/views/add_products.blade.php
@@ -71,36 +71,22 @@
     <script>
         let cat = document.getElementById('cat');
         let subcat = document.getElementById('subcat');
-        cat.addEventListener('change',()=>{
-            document.getElementById('subcat-row').style.visibility="visible";
-            subcat.options[0].selected= true;
-            for(let i = 0; i<subcat.options.length;i++){
-                if(cat.value==subcat.options[i].dataset.parent){
-                    
-                    // if(subcat.options[i].value == "null"){
-                    //     subcat.options[i].style.display="none";
-                    // }
-                    // else{
-                    //     subcat.options[i].style.display="block";
-                    // }
-                    subcat.options[i].style.display="block";
-                    document.getElementById('null-opt').style.display="none !important";            
+
+        cat.addEventListener('change', () => {
+            document.getElementById('subcat-row').style.visibility = "visible";
+            subcat.options[0].selected = true;
+
+            for (let i = 0; i < subcat.options.length; i++) {
+                if (cat.value == subcat.options[i].dataset.parent) {
+                subcat.options[i].style.display = "block";
+                document.getElementById('null-opt').style.display = "none";
+                } else {
+                subcat.options[i].style.display = "none";
+                if (subcat.options[i].value === "null") {
+                    subcat.options[i].style.display = "block";
                 }
-                else{
-                    
-                    subcat.options[i].style.display="none";
-                    document.getElementById('null-opt').style.display="block !important";
-                    // if(subcat.options[i].value == "null"){
-                    //     subcat.options[i].style.display="block";
-                    // }
-                    // else{
-                    //     subcat.options[i].style.display="none";
-                    // }
                 }
-                // console.log(subcat.options[i].dataset.parent);
             }
-        })
-        
-        
+        });
     </script>
 @endsection
