783f719ccc084d516ecef3c6e1092e3516118bdf
willshallC <netcreaterss@gmail.com>
2023-07-18 11:10:18 +0530

    fixed categories dropdown logic on subcategories form

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -21,7 +21,8 @@ class PageController extends Controller
 
     //sub-category Form
     function subCategoryForm(){
-        $categories = DB::table('categories')->select(['id','cat_name'])->get();
+        
+        $categories = DB::table('categories as c')->join('sub_categories as sc','c.id','=','sc.parent_id')->select(['c.cat_name','c.id'])->distinct('c.cat_name')->get();
         return view('add_sub_cat',['categories'=>$categories]);
     }
 
