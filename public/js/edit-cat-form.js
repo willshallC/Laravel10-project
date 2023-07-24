0fb00b98372fb24cd87a2b9e7227464f0fb6bb3c
willshallC <netcreaterss@gmail.com>
2023-07-24 18:40:33 +0530

    fixed JS issue on the category edit form page

--- public/js/edit-cat-form.js
+++ public/js/edit-cat-form.js
@@ -1,24 +1,21 @@
 CKEDITOR.replace('cat_description');
 
-//let cat_names = document.querySelector('form[name=editCatForm] input[name=cat_names]');
- let cat_slug = document.querySelector('form[name=edit-cat] input[name=cat_slug]');
-// let cat_img = document.querySelector('form[name=editCatForm] input[name=cat_img]');
-// let catForm =  document.querySelector('form[name=editCatForm]');
+let cat_name = document.querySelector('form[name=editCatForm] input[name=cat_name]');
+let cat_slug = document.querySelector('form[name=editCatForm] input[name=cat_slug]');
+let cat_img = document.querySelector('form[name=editCatForm] input[name=cat_img]');
+let catForm =  document.querySelector('form[name=editCatForm]');
 
-console.log(cat_slug);
-// console.log(catForm);
-// cat_names.addEventListener('change',()=>{
+cat_name.addEventListener('change',()=>{
     
-//     slug = cat_names.value;
-//     slug = slug.trim();
-//     slug = slug.replace(/[\W_]+$/,'');
-//     slug = slug.toLowerCase();
-//     slug = slug.replace(/[^a-zA-Z0-9]/g,'-');
-//     cat_slug.value = slug;
-//     alert(cat_names.value);
-// })
+    slug = cat_name.value;
+    slug = slug.trim();
+    slug = slug.replace(/[\W_]+$/,'');
+    slug = slug.toLowerCase();
+    slug = slug.replace(/[^a-zA-Z0-9]/g,'-');
+    cat_slug.value = slug;
+})
 
-// function editCat(event){
-//     alert();
-//     event.preventDefault();
-// }
+function editCat(event){
+    alert();
+    event.preventDefault();
+}
--- resources/views/view_category.blade.php
+++ resources/views/view_category.blade.php
@@ -1,64 +1,59 @@
 @extends('layouts.masterlayout')
 
 @section('content')
-    <table>
-        <form name="edit-cat">
-            <tr>
-                <th>ID:</th>
-                <td><input type="number" value="{{$category->id}}" name="cat_id" readonly/></td>
-            </tr>
-            <tr>
-                <th>Name:</th>
-                <td><input type="text" name="cat_names" value="{{$category->cat_name}}"/></td>
-            </tr>
-            <tr>
-                <th>Slug:</th>
-                <td><input type="text" value="{{$category->cat_slug}}" name="cat_slug" minlength="3"/></td>
-            </tr>
-            <tr>
-                <th>Status:</th>
-                <td>
-                    @if ($category->cat_status==1)
-                        Active <input checked type="radio" name="cat_status" value="1"/>
-                        Inactive <input type="radio" name="cat_status" value="0"/>
-                    @else
-                        Active <input type="radio" name="cat_status" value="1"/>
-                        Inactive <input checked type="radio" name="cat_status" value="0"/>
-                    @endif
-                </td>
-            </tr>
-            <tr>
-                <th>Top Category:</th>
-                <td>
-                    @if ($category->top_cat==1)
-                        Yes <input type="radio" name="top_cat" checked value="1"/>
-                        No <input type="radio" name="top_cat" value="0"/>
-                    @else
-                        Yes <input type="radio" name="top_cat" value="1"/>
-                        No <input type="radio" name="top_cat" value="0" checked/>
-                    @endif
-                </td>
-            </tr>
-            <tr>
-                <th>Image:</th>
-                <td><input type="text" value="{{$category->cat_img}}" name="cat_img"/></td>
-            </tr>
-            <tr>
-                <th>Description</th>
-                <td><textarea name="cat_description" >{{$category->cat_description}}</textarea></td>
-            </tr>
-            <tr>
-                <td><input type="button"  value="Editgdfg" onclick="editCat(event)"/></td>
-            </tr>
-        </form>
-    </table>
-    <script>
-         let cat_slug = document.querySelector('form[name=edit-cat] input[name=cat_slug]');
-// let cat_img = document.querySelector('form[name=editCatForm] input[name=cat_img]');
-// let catForm =  document.querySelector('form[name=editCatForm]');
-
-console.log(cat_slug);
-    </script>
+    <form name="editCatForm">
+        <table>
+            
+                <tr>
+                    <th>ID:</th>
+                    <td><input type="number" value="{{$category->id}}" name="cat_id" readonly/></td>
+                </tr>
+                <tr>
+                    <th>Name:</th>
+                    <td><input type="text" name="cat_name" value="{{$category->cat_name}}"/></td>
+                </tr>
+                <tr>
+                    <th>Slug:</th>
+                    <td><input type="text" value="{{$category->cat_slug}}" name="cat_slug" minlength="3"/></td>
+                </tr>
+                <tr>
+                    <th>Status:</th>
+                    <td>
+                        @if ($category->cat_status==1)
+                            Active <input checked type="radio" name="cat_status" value="1"/>
+                            Inactive <input type="radio" name="cat_status" value="0"/>
+                        @else
+                            Active <input type="radio" name="cat_status" value="1"/>
+                            Inactive <input checked type="radio" name="cat_status" value="0"/>
+                        @endif
+                    </td>
+                </tr>
+                <tr>
+                    <th>Top Category:</th>
+                    <td>
+                        @if ($category->top_cat==1)
+                            Yes <input type="radio" name="top_cat" checked value="1"/>
+                            No <input type="radio" name="top_cat" value="0"/>
+                        @else
+                            Yes <input type="radio" name="top_cat" value="1"/>
+                            No <input type="radio" name="top_cat" value="0" checked/>
+                        @endif
+                    </td>
+                </tr>
+                <tr>
+                    <th>Image:</th>
+                    <td><input type="text" value="{{$category->cat_img}}" name="cat_img"/></td>
+                </tr>
+                <tr>
+                    <th>Description</th>
+                    <td><textarea name="cat_description" >{{$category->cat_description}}</textarea></td>
+                </tr>
+                <tr>
+                    <td><input type="button"  value="Editgdfg" onclick="editCat(event)"/></td>
+                </tr>
+            
+        </table>
+    </form>
 @endsection
 
 @push('scripts')
