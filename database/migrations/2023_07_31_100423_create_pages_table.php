86251d2be23279454bc38f3ff6ab36036de5c402
willshallC <netcreaterss@gmail.com>
2023-07-31 16:13:03 +0530

    created pages and templates migration

---
+++ database/migrations/2023_07_31_100423_create_pages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Brick\Math\BigInteger;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',40)->nullable(false)->unique();
+            $table->string('slug',40)->nullable(false)->unique();
+            $table->string('description',50000)->nullable(false);
+            $table->string('image',100);
+            $table->unsignedBigInteger('page_template');
+            $table->string('seo_title',100)->nullable();
+            $table->string('meta_description',100)->nullable();
+            $table->boolean('published')->default(1)->nullable(false);
+            $table->string('page_schema',2000)->nullable();
+            $table->string('seo_image',100)->nullable();
+            $table->boolean('indexed')->default(0)->nullable(false);
+            $table->foreign('page_template')->references('id')->on('templates')->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pages');
+    }
+};
---
+++ resources/views/create_page.blade.php
@@ -0,0 +1,15 @@
+@extends('admin-layout.admin_layout')
+
+@push('style')
+    <link rel="stylesheet" type="text/css" href="css/admin-style/create-page.css"/>
+@endpush
+
+@section('content')
+    <form>
+        <table>
+            <tr>
+                <th></th>
+            </tr>
+        </table>
+    </form>
+@endsection
