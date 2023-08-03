c64dae5d4b159ca34e33b272a808b0b85d201862
willshallC <netcreaterss@gmail.com>
2023-08-03 13:55:20 +0530

    created blog migration

--- database/migrations/2023_08_03_053048_create_blogs_table.php
+++ database/migrations/2023_08_03_053048_create_blogs_table.php
@@ -13,7 +13,22 @@ return new class extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->string('title',200);
+            $table->string('slug',50)->unique();
+            $table->string('excerpt',100)->nullable();
+            $table->string('description',40000);
+            $table->string('image',100)->nullable();
+            $table->unsignedBigInteger('author');
+            $table->unsignedBigInteger('category');
+            $table->string('seo_title',100)->nullable();
+            $table->string('seo_image',100)->nullable();
+            $table->string('meta_description',700)->nullable();
+            $table->boolean('featured')->default(0);
+            $table->boolean('index')->default(0);
+            $table->boolean('status')->default(1);
             $table->timestamps();
+            $table->foreign('author')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreign('category')->references('id')->on('categories')->cascadeOnDelete()->cascadeOnUpdate();
         });
     }
 
